b7d5f178c3048c6ef563f1b22326a9f52e95db06
Ryan <26681130+MaironI@users.noreply.github.com>
2019-12-03 19:25:54 +0000

    updates main.py syntax
    - Added Splitters for easier viewing of password strings.
    - Added While Loop with if & elif statements to return to main menu if users wishes to continue.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from progress.bar import IncrementalBar
 def randomString(stringLength=10):
     letters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(letters) for i in range(stringLength))
+while True:
 ans = input('How long should your password be?'
             """
             16 bits
@@ -21,16 +22,26 @@ if ans == '16':
         #incbar.next()
     #incbar.finish()
     print('Your 16 Bit Password is \n', randomString(16))
+    print('=======================================================')
 elif ans == '32':
     print('Your 32 Bit Password is \n', randomString(32))
+    print('=======================================================')
 elif ans == '64':
     print('Your 64 Bit Password is \n', randomString(64))
+    print('=======================================================')
 elif ans == '128':
     print('Your 128 Bit Password is \n', randomString(128))
+    print('=======================================================')
 elif ans == '256':
     print('Your 256 Bit Password is \n', randomString(256))
+    print('=======================================================')
 elif ans == '512':
     print('Your 512 Bit Password is \n', randomString(512))
+    print('=======================================================')
+if ans == 'y':
+    continue
+elif ans == 'n':
+    break
 
 
 
